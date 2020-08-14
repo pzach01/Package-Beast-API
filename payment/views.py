e5b2ce548d37e6f05b66ef5e5a7a6ca9235e9230
lucaszach <Lucas Zach>
2020-08-14 14:36:24 -0500

    Spurious commit

--- payment/views.py
+++ payment/views.py
@@ -16,6 +16,7 @@ from rest_framework.decorators import api_view
 import stripe
 stripe.api_key = 'sk_test_51HB4dCJWFTMXIZUo5d1tlWus4t0NGBLPI6LqHVokCzOyXaYZ6f8rcBqAeWZUdtfdc6tl5EenjpUXWrpFsyRmAwgJ00fRuOxc8b'
 import os
+# spurious commit
 @csrf_exempt
 def my_webhook_view(request):
   webhook_secret = os.getenv('STRIPE_WEBHOOK_SECRET')

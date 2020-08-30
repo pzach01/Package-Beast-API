ff774d648d6cb2def5211b3c7597a328fba8dbf5
lucaszach <Lucas Zach>
2020-08-29 21:03:26 -0500

    Made migrations and added max_length field to stripeInvoiceIds

--- subscription/models.py
+++ subscription/models.py
@@ -32,7 +32,7 @@ class Subscription(models.Model):
     stripeSubscriptionItemDataPriceId=models.CharField(max_length=50)
     stripeSubscriptionCurrentPeriodEnd=models.CharField(max_length=50)
     stripeSubscriptionCustomer=models.CharField(max_length=50)
-    stripeInvoiceIds=models.CharField(default='')
+    stripeInvoiceIds=models.CharField(default='', max_length=250)
     '''
     subscriptionType=models.CharField(max_length=20)
     numRequestsLeft=models.IntegerField()

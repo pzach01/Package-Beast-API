6234766156b0b528189eaa9429c6dbd3b94205c5
lucaszach <Lucas Zach>
2020-09-10 11:56:42 -0500

    Corrected endpoint for getSubscriptionInfo

--- payment/urls.py
+++ payment/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('payment/stripeInvoice/', views.my_webhook_view),
     path('payment/createStripeSubscription/', views.create_stripe_subscription),
     path('payment/retryStripeSubscription/', views.retry_stripe_subscription),
-    path('payment/userHasStripeSubscription/',views.user_has_stripe_subscription),
+    path('payment/getSubscriptionInfo/',views.get_subscription_info),
     path('payment/cancelStripeSubscription/',views.cancel_subscription),
     path('payment/updateStripeSubscription/',views.update_stripe_subscription),
 ]

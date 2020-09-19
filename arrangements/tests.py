3f9105c10aea2005d6a002b1e2d04677fbeea144
pzach01 <peter.douglas.zach@gmail.com>
2020-09-18 20:25:33 -0400

    modified test

--- arrangements/tests.py
+++ arrangements/tests.py
@@ -59,23 +59,39 @@ def test_1():
 
     from rest_framework.test import APIClient, APIRequestFactory
     from rest_framework.authtoken.models import Token
+    from arrangements.views import ArrangementList, Arrangement
 
 
     client = APIClient()
     from users.models import User
     
     factory = APIRequestFactory()
-    request = factory.post('/arrangments/',inputData)
+    # request = factory.post('/arrangments/',inputData)
 
-    user = User.objects.get(email='lucas.j.zach@gmail.com')
-    client = APIClient()
-    client.login(username='lucas.j.zach@gmail.com', password='Letsgetit35!')
-    tokens = Token.objects.all()
-    token=tokens[len(tokens)-1]
-    print(tokens)
-    client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+    # user = User.objects.get(email='lucas.j.zach@gmail.com')
+    # client = APIClient()
+    # client.login(username='lucas.j.zach@gmail.com', password='Letsgetit35!')
+    # print("yoyo")
+    # tokens = Token.objects.all()
+
+    from rest_framework.test import force_authenticate
+
+    pz_user = User.objects.get(email='peter.douglas.zach@gmail.com')
+    pz_view = ArrangementList.as_view()
+
+    print(pz_user)
+    print(pz_view)
+    # Make an authenticated request to the view...
+    pz_request = factory.get('/arrangements/')
+    force_authenticate(pz_request, user=pz_user)
+    pz_response = pz_view(pz_request)
+    print("pz_response", pz_response)
+
+    # token=tokens[len(tokens)-1]
+    # print(tokens)
+    # client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
 
 
-    response=client.post('/arrangments/', request, format='json')
-    print(response)
+    # response=client.post('/arrangments/', request, format='json')
+    # print(response)
 test_1()

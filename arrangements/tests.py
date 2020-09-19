90e22cb62542344be268cb0c85880f42d36bb0d0
lucaszach <Lucas Zach>
2020-09-18 21:03:40 -0500

    Very simple test working for arrangments

--- arrangements/tests.py
+++ arrangements/tests.py
@@ -55,43 +55,39 @@ def test_1():
         }
     ]
     }
-    # lowercase post returns byte code this returns subscriptable object
 
     from rest_framework.test import APIClient, APIRequestFactory
     from rest_framework.authtoken.models import Token
     from arrangements.views import ArrangementList, Arrangement
+    from rest_framework.test import force_authenticate
 
+    import json
+    from users.models import User
 
     client = APIClient()
-    from users.models import User
-    
     factory = APIRequestFactory()
-    # request = factory.post('/arrangments/',inputData)
 
-    # user = User.objects.get(email='lucas.j.zach@gmail.com')
-    # client = APIClient()
-    # client.login(username='lucas.j.zach@gmail.com', password='Letsgetit35!')
-    # print("yoyo")
-    # tokens = Token.objects.all()
-
-    from rest_framework.test import force_authenticate
 
     pz_user = User.objects.get(email='peter.douglas.zach@gmail.com')
     pz_view = ArrangementList.as_view()
 
-    print(pz_user)
-    print(pz_view)
-    # Make an authenticated request to the view...
-    pz_request = factory.get('/arrangements/')
+    pz_request = factory.post('/arrangements/',json.dumps(inputData), content_type='application/json')
     force_authenticate(pz_request, user=pz_user)
     pz_response = pz_view(pz_request)
-    print("pz_response", pz_response)
+    data=pz_response.data
+    
 
-    # token=tokens[len(tokens)-1]
-    # print(tokens)
-    # client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+    # checks on the data, will print -----------Failed Arrangments Test 1 if these fail
+
+    assert(data['arrangementPossible']==True)
+    assert(data['multiBinPack']==False)
 
 
-    # response=client.post('/arrangments/', request, format='json')
-    # print(response)
-test_1()
+try:
+    test_1()
+    print('Passed Arrangments Test 1')
+except:
+    print('------------Failed Arrangments Test 1')
+ 
+
+

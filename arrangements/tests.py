2c8ce2ddc03c0c23e2b22778433eedd34d1bd3e3
lucaszach <Lucas Zach>
2020-09-20 19:31:45 -0500

    Updated tests and new tests to reflect new serializer design

--- arrangements/tests.py
+++ arrangements/tests.py
@@ -181,18 +181,22 @@ def test_2(client):
     data=pz_response.data
     
 
-    # checks on the data, will print -----------Failed Arrangments Test 1 if these fail
+    # checks on the data, will print -----------Failed Arrangments Test 2 if these fail
 
     assert(data['arrangementPossible']==False)
     assert(data['multiBinPack']==False)
-
+    assert(len(data['items'])==5)
+    for item in data['items']:
+        # null container field
+        assert(item['container']==0)
+    assert(len(data['containers'])==1)
     return client
 
 
 
 
 # QUESTION ASKED: what is the behavior when we try to optimize boxes into multiple containers (and there is one best container)
-# only return containers that are filled
+# return all containers that are filled
 def test_3(client):
     inputData={
     "multiBinPack": False,
@@ -274,12 +278,159 @@ def test_3(client):
 
     assert(data['arrangementPossible']==True)
     assert(data['multiBinPack']==False)
-    # only include containers that are filled
-    assert(len(data['containers'])==1)
-    usedContainerId=data['containers'][0]['id']
+    assert(len(data['items'])==4)
+    assert(len(data['containers'])==2)
+    usedContainerId=None
+    for container in data['containers']:
+        if container['xDim']==2 and (container['yDim']==2 and container['zDim']==1):
+            usedContainerId=container['id']
     for item in data['items']:
         assert(item['container']==usedContainerId)
     return client
+
+# QUESTION ASKED: does the items returned match the order of the rendering (in at least one case)
+def test_4(client):
+    inputData={
+    "multiBinPack": False,
+    "timeoutDuration": 30,
+    "containers": [
+        {
+        "sku": "unit",
+        "description": "string",
+        "xDim": 2,
+        "yDim": 2,
+        "zDim": 2,
+        "units": "in"
+        }
+    ],
+    "items": [
+        {
+        "id": 0,
+        "sku": "string",
+        "description": "string",
+        "length": 1,
+        "width": 1,
+        "height": 1,
+        "units": "in"
+        },
+        {
+        "id": 1,
+        "sku": "string",
+        "description": "string",
+        "length": 1,
+        "width": 1,
+        "height": 1,
+        "units": "in"
+        },
+        {
+        "id": 2,
+        "sku": "string",
+        "description": "string",
+        "length": 1,
+        "width": 1,
+        "height": 1,
+        "units": "in"
+        },
+        {
+        "id": 3,
+        "sku": "string",
+        "description": "string",
+        "length": 1,
+        "width": 1,
+        "height": 1,
+        "units": "in"
+        },
+        {
+        "id": 4,
+        "sku": "string",
+        "description": "string",
+        "length": 1,
+        "width": 1,
+        "height": 1,
+        "units": "in"
+        },
+        {
+        "id": 5,
+        "sku": "string",
+        "description": "string",
+        "length": 1,
+        "width": 1,
+        "height": 1,
+        "units": "in"
+        },
+        {
+        "id": 6,
+        "sku": "string",
+        "description": "string",
+        "length": 1,
+        "width": 1,
+        "height": 1,
+        "units": "in"
+        },
+        {
+        "id": 7,
+        "sku": "string",
+        "description": "string",
+        "length": 1,
+        "width": 1,
+        "height": 1,
+        "units": "in"
+        }
+    ]
+    }
+
+
+
+    factory = APIRequestFactory()
+
+
+    pz_user = User.objects.get(email='peter.douglas.zach@gmail.com')
+    pz_view = ArrangementList.as_view()
+
+    pz_request =factory.post('/arrangements/',data=inputData, format='json')
+
+    force_authenticate(pz_request, user=pz_user)
+    pz_response = pz_view(pz_request)
+    data=pz_response.data
+    
+
+    # checks on the data, will print -----------Failed Arrangments Test 1 if these fail
+
+    assert(data['arrangementPossible']==True)
+    assert(data['multiBinPack']==False)
+
+
+    selectedContainer=data['containers'][0]['id']
+    for item in data['items']:
+        assert(item['container']==selectedContainer)
+
+    
+    item1Tuple=(data['items'][0]['xCenter'],data['items'][0]['yCenter'],data['items'][0]['zCenter'])
+    assert(item1Tuple==(.5,.5,.5))
+    
+    item2Tuple=(data['items'][1]['xCenter'],data['items'][1]['yCenter'],data['items'][1]['zCenter'])
+    assert(item2Tuple==(.5,.5,1.5))
+
+    item3Tuple=(data['items'][2]['xCenter'],data['items'][2]['yCenter'],data['items'][2]['zCenter'])
+    assert(item3Tuple==(.5,1.5,.5))
+    
+    item4Tuple=(data['items'][3]['xCenter'],data['items'][3]['yCenter'],data['items'][3]['zCenter'])
+    assert(item4Tuple==(.5,1.5,1.5))
+
+    item5Tuple=(data['items'][4]['xCenter'],data['items'][4]['yCenter'],data['items'][4]['zCenter'])
+    assert(item5Tuple==(1.5,.5,.5))
+
+    item6Tuple=(data['items'][5]['xCenter'],data['items'][5]['yCenter'],data['items'][5]['zCenter'])
+    assert(item6Tuple==(1.5,.5,1.5))
+
+    item7Tuple=(data['items'][6]['xCenter'],data['items'][6]['yCenter'],data['items'][6]['zCenter'])
+    assert(item7Tuple==(1.5,1.5,.5))
+
+    item8Tuple=(data['items'][7]['xCenter'],data['items'][7]['yCenter'],data['items'][7]['zCenter'])
+    assert(item8Tuple==(1.5,1.5,1.5))
+    
+    return client
+
 
 
 
@@ -307,3 +458,10 @@ try:
     print('Passed Arrangments Test 3')
 except:
     print('------------Failed Arrangments Test 3')
+
+
+try:
+    client=test_4(client)
+    print('Passed Arrangments Test 4')
+except:
+    print('------------Failed Arrangments Test 4')

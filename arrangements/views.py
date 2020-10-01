1bbdc1f915a722fb4e399cfcdf7c9a3d50cb0625
pzach01 <peter.douglas.zach@gmail.com>
2020-10-01 11:13:51 -0400

    removed extra line

--- arrangements/views.py
+++ arrangements/views.py
@@ -11,7 +11,6 @@ class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj.owner == request.user
 
-
 class ArrangementList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticated, IsOwner]
     def get_queryset(self):

e5d0dd02312377bcf6b1cfbf1b82a49fc1b23bcf
pzach01 <peter.douglas.zach@gmail.com>
2021-01-04 15:30:10 -0500

    removed dummyField

--- items/serializers.py
+++ items/serializers.py
@@ -8,9 +8,9 @@ class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = ['id', 'container', 'arrangement', 'sku', 'description', 'length', 'width', 'height', 'xDim',
-                  'yDim', 'zDim', 'volume', 'xCenter', 'yCenter', 'zCenter', 'units', 'masterItemId','dummyField']
+                  'yDim', 'zDim', 'volume', 'xCenter', 'yCenter', 'zCenter', 'units', 'masterItemId']
         read_only_fields = ['container', 'arrangement',
-                            'volume', 'xCenter', 'yCenter', 'zCenter', 'xDim', 'yDim', 'zDim', 'masterItemId','dummyField']
+                            'volume', 'xCenter', 'yCenter', 'zCenter', 'xDim', 'yDim', 'zDim', 'masterItemId']
 
     def create(self, validated_data):
         userSubscription=Subscription.objects.filter(owner=validated_data['owner'])[0]

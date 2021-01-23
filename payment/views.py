71b5a89c8171199ecec5ce5606bbe78860cc8b8e
lucaszach <Lucas Zach>
2021-01-23 13:54:05 -0600

    Added test tax rate to create subscription

--- payment/views.py
+++ payment/views.py
@@ -249,6 +249,8 @@ def create_stripe_subscription(request):
             ],
             expand=['latest_invoice.payment_intent'],
             payment_behavior='error_if_incomplete',
+            default_tax_rates=['txr_1ICrg3E5mpXPYa9nYpguvzzc'],
+
 
         )
     except stripe.error.CardError as e:

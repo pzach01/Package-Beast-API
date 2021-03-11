bb96474e927b4781265461be38c099e5fce3e0aa
pzach01 <peter.douglas.zach@gmail.com>
2021-03-11 09:37:40 -0500

    added check for account and connector

--- Package-Beast-API/urls.py
+++ Package-Beast-API/urls.py
@@ -35,12 +35,47 @@ from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from rest_auth.registration.views import SocialLoginView
 from subscription.models import Subscription
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
-
+from allauth.account.models import EmailAddress
 
 class GoogleLogin(SocialLoginView):
     adapter_class = GoogleOAuth2Adapter
 
 class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
+    def pre_social_login(self, request, sociallogin):
+        """
+        Invoked just after a user successfully authenticates via a
+        social provider, but before the login is actually processed
+        (and before the pre_social_login signal is emitted).
+
+        We're trying to solve different use cases:
+        - social account already exists, just go on
+        - social account has no email or email is unknown, just go on
+        - social account's email exists, link social account to existing user
+        """
+
+        # Ignore existing social accounts, just do this stuff for new ones
+        if sociallogin.is_existing:
+            return
+
+        # some social logins don't have an email address, e.g. facebook accounts
+        # with mobile numbers only, but allauth takes care of this case so just
+        # ignore it
+        if 'email' not in sociallogin.account.extra_data:
+            return
+
+        # check if given email address already exists.
+        # Note: __iexact is used to ignore cases
+        try:
+            email = sociallogin.account.extra_data['email'].lower()
+            email_address = EmailAddress.objects.get(email__iexact=email)
+
+        # if it does not, let allauth take care of this new social account
+        except EmailAddress.DoesNotExist:
+            return
+
+        # if it does, connect this new social login to the existing user
+        user = email_address.user
+        sociallogin.connect(request, user)
     def save_user(self, request, sociallogin, form=None):
         user = super(CustomSocialAccountAdapter, self).save_user(request, sociallogin, form)
         Subscription.objects.create_subscription(user)

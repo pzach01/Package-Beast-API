8b27383514c262006e167668d9949954bf1d7188
pzach01 <peter.douglas.zach@gmail.com>
2021-03-14 13:55:53 -0400

    added weightUnits to User

--- users/serializers.py
+++ users/serializers.py
@@ -82,5 +82,5 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['id', 'email', 'first_name', 'last_name', 'arrangements', 'units', 'dateTimeFormat', 'multiBinPack', 'disableFillContainerAnimation', 'disablePreviousNextItemAnimation', 'animationSpeed', 'usersTermsOfServiceRevision', 'termsOfServiceRevision']
+        fields = ['id', 'email', 'first_name', 'last_name', 'arrangements', 'units', 'weightUnits', 'dateTimeFormat', 'multiBinPack', 'disableFillContainerAnimation', 'disablePreviousNextItemAnimation', 'animationSpeed', 'usersTermsOfServiceRevision', 'termsOfServiceRevision']
         read_only_fields = ['arrangements', 'email', 'termsOfServiceRevision']

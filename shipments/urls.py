efc1f8c4f7ee94c8b00ca6217ad667c005e67d58
lucaszach <Lucas Zach>
2021-07-05 18:40:24 -0500

    Partial progress on setting oauth token

--- shipments/urls.py
+++ shipments/urls.py
@@ -5,4 +5,5 @@ from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = [
     path('shipments/', views.ShipmentList.as_view()),
     path('shipments/<int:pk>/', views.ShipmentDetail.as_view())
+    path('shippo-oauth-access-token/',views.generate_shippo_oauth_token)
 ]
--- shipments/views.py
+++ shipments/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render
-
+import os
 # Create your views here.
 from shipments.models import Shipment
 from shipments.serializers import ShipmentSerializer
 from rest_framework import generics, viewsets, permissions
-
+import requests
 
 class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj.owner == request.user
 
+@api_view(['post'])
+@permission_classes([permissions.IsAuthenticated])
+def generate_shippo_oauth_token(request):
+    shippoRequest={}
+    shippoRequest['client_id']=os.getenv('SHIPPO_CLIENT_ID')
+    shippoRequest['client_secret']=os.getenv('SHIPPO_CLIENT_SECRET')
+    shippoRequest['code']=request.data['code']
+    shippoRequest['grant_type']='authorization_code'
+    resp = requests.post('https://goshippo.com/oauth/access_token', data=shippoRequest)
+    # can't do any additional data processing until I know what the response looks like
+    return resp.json()
 
 class ShipmentList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticated, IsOwner]

2af7200d9dc1ead7c911e1d8bd0f7efa8e91e993
pzach01 <peter.douglas.zach@gmail.com>
2021-07-18 12:15:39 -0400

    new model: shippo transaction

--- shipposervice/models.py
+++ shipposervice/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib import admin
 
 # Create your models here.
+
+class ShippoTransaction(models.Model):
+    owner = models.ForeignKey(
+        'users.User', related_name='shippo_transaction', on_delete=models.CASCADE)
+    label_url = models.CharField(max_length=255, default='', null=True, blank=True)
+admin.site.register(ShippoTransaction)
---
+++ shipposervice/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from shipposervice.models import ShippoTransaction
+
+class ShippoTransactionSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.email')
+
+    class Meta:
+        model = ShippoTransaction
+        depth = 1 #this setting expands the depth of the serialized fields
+        fields = ['id', 'owner', 'label_url']
+        read_only_fields = ['owner']

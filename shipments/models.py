2f52e9808d8aee676a4cebd5d94f6e8810a83473
pzach01 <peter.douglas.zach@gmail.com>
2022-08-04 20:06:33 -0400

    remove dummy field

--- shipments/models.py
+++ shipments/models.py
@@ -29,5 +29,4 @@ class Shipment(SafeDeleteModel):
     noErrorsMakingRequests=models.BooleanField(default=True)
     noValidRequests=models.BooleanField(default=False)
     calculationInProgress = models.BooleanField(default=False, null=True)
-    dummyField = models.BooleanField(default=False, null=True)
 admin.site.register(Shipment)

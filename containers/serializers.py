ca20c8c71cab5254caaa01a628f9c2ee0334863c
pzach01 <peter.douglas.zach@gmail.com>
2022-12-06 07:59:14 -0500

    added volume calculation for analysed containers

--- containers/serializers.py
+++ containers/serializers.py
@@ -11,6 +11,25 @@ class AnalysedContainerSerializer(serializers.ModelSerializer):
         read_only_fields = ['id', 'shipment', 'sku', 'description',
                   'xDim', 'yDim', 'zDim', 'volume', 'units']
 
+    def create(self, validated_data):
+        container = AnalysedContainer.objects.create(**validated_data)
+        container.volume = validated_data['xDim'] * \
+            validated_data['yDim']*validated_data['zDim']
+        container.save()
+        return container
+
+    def update(self, instance, validated_data):
+        instance.sku = validated_data.get('sku', instance.sku)
+        instance.description = validated_data.get(
+            'description', instance.description)
+        instance.xDim = validated_data.get('xDim', instance.xDim)
+        instance.yDim = validated_data.get('yDim', instance.yDim)
+        instance.zDim = validated_data.get('zDim', instance.zDim)
+        instance.volume = instance.xDim * instance.yDim * instance.zDim
+        instance.units = validated_data.get('units', instance.units)
+        instance.save()
+        return instance
+
 class ContainerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Container

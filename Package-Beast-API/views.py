817d09ca2a9766ad920b1a9f0122d659adceefda
pzach01 <peter.douglas.zach@gmail.com>
2023-01-20 22:09:01 -0500

    try some stuff

--- Package-Beast-API/views.py
+++ Package-Beast-API/views.py
@@ -73,7 +73,8 @@ def verify_identity_token(request):
         # if it does not, let allauth take care of this new social account
         except:
             from django.utils.crypto import get_random_string
-            user = User.objects.create_user(email=email, password=get_random_string(length=32), first_name=given_name, last_name=family_name)
+            p = get_random_string(length=10)
+            user = User.objects.create_user(email=email, password=p, first_name=given_name, last_name=family_name)
             Subscription.objects.create_subscription(user)
             user.save()
 
